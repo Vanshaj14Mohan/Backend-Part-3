59a368916adab85f3bbcddff0e835dddd42afa1a
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-04-27 02:16:01 +0530

    Insta Activity

--- InstaPage/index.js
+++ InstaPage/index.js
@@ -4,9 +4,33 @@ const app = express();
 
 let port = 8080;
 
+const path = require("path");
+
+const instaData = require("./data.json"); //requring data.json file, instaData -> JavaScript Object here.
+
+app.set("view engine", "views");
+app.set("views", path.join(__dirname, "views"));
+
 app.get("/", (req,res)=>{
     res.send("You contacted to root path");
 });
+
+app.get("/ig/:username", (req,res)=>{
+    let {username} = req.params; //deconstructiong object;
+    const data = instaData[username];
+    if (data){
+        res.render("insta.ejs", {data});
+    }
+    else{
+        res.render("error.ejs");
+    };
+    // res.render("insta.ejs", {data});
+    // console.log(instaData);
+    console.log(data);
+});
+// app.get("/intro", (req,res)=>{
+//     res.render("page.ejs");
+// });
 
 app.listen(port, ()=>{
     console.log(`App is running on ${port}`);
